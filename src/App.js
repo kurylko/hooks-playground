40b4d122b5f90f1a66ea35202197b5d436fa0bcc
kurylko <reviliya@gmail.com>
2024-01-14 13:27:22 +0100

    using spread operator

--- src/App.js
+++ src/App.js
@@ -1,17 +1,74 @@
 import {useState} from "react";
 
-
 function App() {
-    useState(false);
-    console.log(useState(false));
+    const [activated, setActivated] = useState(false);
+    const [count, setCount] = useState(0);
+    const [address, setAddress] = useState({
+        city: '',
+        country: '',
+    });
 
-  return (
-      <div className="App"> Hello!
-          <br/>
-          <button>Text!</button>
-      </div>
+    // as we are changing not the whole state, need to use spread operator: ...address
+    function handleCityChange(event) {
+        setAddress({...address, city: event.target.value});
+    }
 
-  );
+    function handleCountryChange(event) {
+       setAddress({...address, country: event.target.value});
+    }
+
+    const buttonText = activated ? 'Active' : 'Inactive';
+
+    //const onClick = () => setActivated(!activated);
+    function onClick() {
+        setActivated(!activated);
+    }
+
+    //function handleClickCounter() {
+    //    setCount(count + 1);
+    //}
+
+    const handleClickCounter = () => setCount((prevCount) => {
+        return prevCount + 1;
+    });
+
+    const handleClickCounterDecrease = () => setCount((prevCount) => {
+        return prevCount - 1;
+    });
+
+    const handleClickReset = () => setCount(0);
+
+    return (
+        <div className="App"> Hello!
+            <br/>
+            <button onClick={onClick}>
+                {buttonText}
+            </button>
+            <button
+                onClick={handleClickCounter}>Increase
+            </button>
+            <h2>{count}</h2>
+            <button onClick={handleClickCounterDecrease}>Decrease</button>
+            <button onClick={handleClickReset}>Reset counter</button>
+            <div>
+                <input
+                    type='text'
+                    placeholder='Your city'
+                    value={address.city}
+                    onChange={handleCityChange}
+                ></input>
+            </div>
+            <div>
+                <input
+                    type='text'
+                    placeholder='Your country'
+                    value={address.country}
+                    onChange={handleCountryChange}
+                ></input>
+            </div>
+            <div>U live in {`${address.country}, ${address.country}`}</div>
+        </div>
+    );
 }
 
 export default App;
