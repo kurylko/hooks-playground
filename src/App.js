e2fa04e0cbdd24e5021689d5e51eba50e9b956de
kurylko <reviliya@gmail.com>
2024-01-26 14:57:34 +0100

    event listners

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import * as events from "events";
 
 function App() {
     const [visible, setVisible] = useState(false);
+    const [userText, setUserText] = useState("");
 
     const handleClick = (event) => {
         const {clientX, clientY} = event;
@@ -13,11 +14,28 @@ function App() {
     window.addEventListener("click", handleClick);
     window.removeEventListener("click", handleClick);
 
+    function handleUserKeyPress(event) {
+        const {key, keyCode} = event;
+        if (keyCode === 32 || (keyCode >= 65 && keyCode <= 90)) {
+            setUserText(`${userText} ${key}`);
+        }
+    }
+
+    useEffect(() => {
+        window.addEventListener("keydown", handleUserKeyPress);
+        return () => {
+            window.removeEventListener("keydown", handleUserKeyPress);
+        }
+
+    })
+
     return (
         <div className="App">
-            <button onClick={() => setVisible(!visible )}>
+            <h1>Your text goes here</h1>
+            <blockquote>{userText}</blockquote>
+            <button onClick={() => setVisible(!visible)}>
                 {visible ? "Hide counter" : "Show counter"}</button>
-            {visible && <Counter />}
+            {visible && <Counter/>}
         </div>
     );
 }
