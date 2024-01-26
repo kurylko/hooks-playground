45879ea12e99b58fd32a52a1bd072264df4729ba
kurylko <reviliya@gmail.com>
2024-01-26 14:42:38 +0100

    event listners

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import {useEffect, useState} from "react";
 import Counter from "./components/counter";
+import * as events from "events";
 
 function App() {
     const [visible, setVisible] = useState(false);
+
+    const handleClick = (event) => {
+        const {clientX, clientY} = event;
+        console.log(`Mouse is at ${clientX}, ${clientY}`);
+    }
+
+    window.addEventListener("click", handleClick);
+    window.removeEventListener("click", handleClick);
 
     return (
         <div className="App">
