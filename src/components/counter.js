63f97e4b683afaddce70fcfc342a771152f37c9a
kurylko <reviliya@gmail.com>
2024-01-26 14:33:28 +0100

    second argument

--- src/components/counter.js
+++ src/components/counter.js
@@ -2,6 +2,7 @@ import {useEffect, useState} from "react";
 
 function Counter() {
     const [count, setCount] = useState(0);
+    const [color, setColor] = useState('salmon');
 
     const handleClickCounterIncrease = () => setCount((prevCount) => {
         return prevCount + 1;
@@ -12,23 +13,28 @@ function Counter() {
     });
 
     const handleClickReset = () => setCount(0);
+    function handleColorChange(){
+        const nextColor = color ===  "salmon" ? "gold" : "salmon"
+        setColor(nextColor);
+    }
 
     useEffect(() => {
-        console.log(`Inside useEffect. The current count is ${count}`);
+        console.log(`Inside useEffect. Only run upon component mounting. The current count is ${count}`);
 
         return () => {
-            console.log(`Removing. last count = ${count}`);
+            console.log(`Removing anything. last count = ${count}. Only run when component unmounted`);
         }
-    });
+    }, [color]);
 
     return (
     <div style={{paddingTop: "30px"}}>
         <button
             onClick={handleClickCounterIncrease}>Increase
         </button>
-        <h2>{count}</h2>
+        <h2 style={{color}}>{count}</h2>
         <button onClick={handleClickCounterDecrease}>Decrease</button>
         <button onClick={handleClickReset}>Reset counter</button>
+        <button onClick={handleColorChange}>Change color</button>
     </div>
     )
 }
